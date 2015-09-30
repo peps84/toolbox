96d658d38ddf2a830a127073a00d1ccda8435771
Sam Pepper <sam_pepper@dennis.co.uk>
2015-09-30 14:04:31 +0100

    Almost working, hashres issues still

---
+++ Gruntfile.js
@@ -0,0 +1,56 @@
+	module.exports = function (grunt){
+
+	grunt.initConfig({
+
+	pkg: grunt.file.readJSON('package.json'),
+
+
+
+/////////////////////////////////////////// Hashing
+
+	hashres: {
+		options: {
+			encoding: 'utf8',
+			fileNameFormat: '${hash}.${ext}',
+			renameFiles: true
+		},
+		prod: {
+			src: [
+				// WARNING: These files will be renamed!
+				'*.{png,jpg,gif}'
+			],
+			dest: '/',
+		}
+	},
+
+/////////////////////////////////////////// Uploading
+
+	ftp_push: {
+		your_target: {
+			options: {
+				username: 'superSam',
+				password: 'BTR45cll!',
+				host: 'ftp.sampepper.net',
+				dest: '/friday_funnies',
+				port: 21,
+				debug: true,
+			},
+			files: [
+				{
+					expand: true,
+					cwd: '.',
+					src: ['*.{png,jpg,gif}']
+				}
+			]
+		}
+	}
+});
+	grunt.loadNpmTasks('grunt-hashres');
+	grunt.loadNpmTasks('grunt-ftp-push');
+
+	/////////////////////////////////////////// Default builds the ad from local tamplates and updates attributes from JSON files
+	grunt.registerTask('default', [
+		'hashres:prod',
+		'ftp_push'
+	]);
+};
